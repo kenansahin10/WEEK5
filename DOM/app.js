4f200ce9caf1e7376c48e6528a7396324134d6e7
kenansahin <kenansahin803@gmail.com>
2021-03-10 23:32:19 +0200

    v5

--- DOM/app.js
+++ DOM/app.js
@@ -4,7 +4,7 @@ divFormGrp.className='form-group';
 let lblEmail=document.createElement('label');
 lblEmail.id='lblEmail';
 lblEmail.htmlFor='txtEmail';
-lblEmail.innerHTML='Email Adresinizi Giriniz';
+lblEmail.innerHTML='Email';
 divFormGrp.appendChild(lblEmail);
 let txtEmail=document.createElement('input');
 txtEmail.id='txtEmail';
@@ -14,4 +14,18 @@ txtEmail.placeholder='Email Adresinizi Giriniz';
 divFormGrp.appendChild(txtEmail);
 let divCont = document.querySelector("#divCont");
 divCont.appendChild(divFormGrp);
+
+let lblPassword=document.createElement('label');
+lblPassword.id='pwd';
+lblPassword.htmlFor='txtPassword';
+lblPassword.innerHTML='Sifre';
+divFormGrp.appendChild(lblPassword);
+let txtPassword=document.createElement('input');
+txtPassword.id='txtPassword';
+txtPassword.type='password';
+txtPassword.className='form-control';
+txtPassword.placeholder='Sifrenizi Giriniz';
+divFormGrp.appendChild(txtPassword);
+
+
 

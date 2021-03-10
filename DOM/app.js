641909b1402f687291d9cee84516c910924bd05d
kenansahin <kenansahin803@gmail.com>
2021-03-10 14:28:45 +0200

    dom

--- DOM/app.js
+++ DOM/app.js
@@ -1,134 +1,17 @@
-// console.log(document.title);
-// console.log(document.doctype);
-// console.log(document.body);
-// console.log(document.forms);
-// console.log(document.links);
+let divFormGrp = document.createElement('div');
+divFormGrp.id='divFormGrp';
+divFormGrp.className='form-group';
+let lblEmail=document.createElement('label');
+lblEmail.id='lblEmail';
+lblEmail.htmlFor='txtEmail';
+lblEmail.innerHTML='Email Adresinizi Giriniz';
+divFormGrp.appendChild(lblEmail);
+let txtEmail=document.createElement('input');
+txtEmail.id='txtEmail';
+txtEmail.type='email';
+txtEmail.className='form-control';
+txtEmail.placeholder='Email Adresinizi Giriniz';
+divFormGrp.appendChild(txtEmail);
+let divCont = document.querySelector("#divCont");
+divCont.appendChild(divFormGrp);
 
-//document.getElementById
-//let item = document.getElementById('pwd');
-//input.value = 'Lutfen Sifrenizi Giriniz';
-//input.className = 'form-group';
-
-
-
-// let item = document.getElementById('test');
-
-
-// // console.log(item.textContent);
-// // console.log(item.innerText);
-// // console.log(item.innerHTML);
-
-// item.style.border = 'dashed 2px';
-
-
-
-
-
-//document.getElementsByClassName
-//let items = document.getElementsByClassName('list-group-item');
-
-// items[3].textContent = 'Hello';
-// items[0].style.backgroundColor = 'red';
-
-// for (let index = 0; index < items.length; index++) {
-//     items[index].style.backgroundColor = 'red';
-// }
-
-// console.log(items[3])
-
-
-
-// document.getElementsByTagName
-// let items = document.getElementsByTagName('button');
-// console.log(items[0])
-
-// document.getElementsByName
-
-// let items = document.getElementsByName('check');
-// console.log(items)
-
-
-
-
-
-// document.querySelector
-
-// let item = document.querySelector('.list-group-item:nth-child(2)');
-
-// console.log(item)
-
-// console.log(item.lastChild);
-// console.log(item.lastChild);
-// console.log(item.lastElementChild.style.backgroundColor = 'red');
-
-
-
-// create element
-
-//let button = document.createElement('button');
-// button.className = 'btn btn-success';
-//button.innerHTML = 'Created button';
-// button.setAttribute('id', 'yeni_buton');
-// button.setAttribute('data', 'button_data');
-
-// let div = document.querySelector('#test');
-
-// div.appendChild(button);
-
-// console.log(button)
-
-
-// events
-
-
-
-//function tikla() {
-//    console.log('Butona tiklandi');
-//    alert('asdljasda')
-//}
-
-//let button = document.querySelector('#submit');
-
-//button.addEventListener('click', function (ele) {
-//    console.log(ele.target)
-
-  //  console.log(e.layerX)
-   // console.log(e.layerY)
-   // console.log('funksion calisti')
-//});
-
-
-// let input = document.querySelector('#exampleInputPassword1');
-
-// // input.addEventListener('focus', focus);
-
-// input.addEventListener('change', function (e) {
-
-
-//     console.log(e.target.value)
-// });
-
-// function focus() {
-//     console.log('focus calisti')
-// }
-
-
-//let link = document.querySelector('#test_link');
-
-l//ink.addEventListener('click', function (e) {
-   // console.log(e.target);
-    //e.preventDefault();
-
-    //console.log('link calisti');
-
-    // .....
-    // .....
-    // .....
-    // .....
-    // .....
-    // .....
-    // .....
-    // .....
-
- //   location.href = "http://google.com"
-//});
